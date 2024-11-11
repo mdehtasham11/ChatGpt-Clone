6d03fbab2572ba67fd381894cbc855308baa83eb
mdehtasham11 <md.01219011922@ipu.ac.in>
2024-11-11 20:45:21 +0530

    btn updt

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -13,9 +13,8 @@ const  Header = () => {
         <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
           <span className="ml-3 text-xl">ChatGpt</span>
         </a>
-        <button onClick={deleteData} className="inline-flex items-center border rounded-3xl justify-items-end bg-gray-100 py-1 px-3 focus:outline-none hover:bg-gray-200  text-base mt-4 md:mt-0 w-24">
+        <button onClick={deleteData} className="inline-flex items-center border rounded-3xl justify-items-end bg-gray-400 py-1 px-3 focus:outline-none hover:bg-gray-200  text-base mt-4 md:mt-0">
         <Trash2 />
-        <span className="ml-2 size-6 text-lg">Clear</span>
         </button>
       </div>
       
