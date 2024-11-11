cab698f08df2958754c2ae001ca3c724560ef4bc
mdehtasham11 <md.01219011922@ipu.ac.in>
2024-11-11 20:39:29 +0530

    adding clear button

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,25 +1,21 @@
 import React from "react";
+import { Trash2 } from "lucide-react";
 
 const  Header = () => {
+  
+   function deleteData() {
+    localStorage.removeItem("chat_History");
+    window.location.reload();
+  }
   return (
     <header className="text-gray-600 body-font">
       <div className="container mx-auto flex flex-wrap justify-between  p-5 flex-col md:flex-row items-center">
         <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
           <span className="ml-3 text-xl">ChatGpt</span>
         </a>
-        <button className="inline-flex items-center justify-items-end bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-          Button
-          <svg
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            className="w-4 h-4 ml-1"
-            viewBox="0 0 24 24"
-          >
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
+        <button onClick={deleteData} className="inline-flex items-center border rounded-3xl justify-items-end bg-gray-100 py-1 px-3 focus:outline-none hover:bg-gray-200  text-base mt-4 md:mt-0 w-24">
+        <Trash2 />
+        <span className="ml-2 size-6 text-lg">Clear</span>
         </button>
       </div>
       
