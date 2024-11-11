deec74288bdb870f204b587a2d6f8f43474b291f
mdehtasham11 <md.01219011922@ipu.ac.in>
2024-11-11 19:48:46 +0530

    updating store

--- src/assets/store/store.js
+++ src/assets/store/store.js
@@ -1,20 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const messageSlice = createSlice({
-    name: "message",
-    initialState:{
-        inputMesaage: "",
-        response: "",
-    },
-    reducers:{
-        setInputMesaage:(state,action) =>{
-            state.inputMesaage = action.payload
-        },
-        setResponse: (state,action) =>{
-            state.response = action.payload
-        }
-    }
-})
+const data = JSON.parse(localStorage.getItem("chat_History") || "[]");
 
-export const { setInputMesaage, setResponse} = messageSlice.actions;
+const messageSlice = createSlice({
+  name: "message",
+  initialState: {
+    storedMessage: data || [],
+    inputMessage: "",
+    response: "",
+  },
+  reducers: {
+    setInputMessage: (state, action) => {
+      state.inputMessage = action.payload;
+    },
+    setResponse: (state, action) => {
+      state.response = action.payload;
+
+      const newEntry = {
+        message: state.inputMessage,
+        response: state.response,
+      };
+
+      state.storedMessage.push(newEntry);
+      localStorage.setItem("chat_History", JSON.stringify(state.storedMessage));
+      // alert(state.storedMessage);
+    },
+  },
+});
+
+export const { setInputMessage, setResponse } = messageSlice.actions;
 export default messageSlice.reducer;
